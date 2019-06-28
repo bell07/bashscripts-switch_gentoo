51243b5f58fec6c0d6efdf6b3316cec38afaaa7c
Alexander Weber <web.alexander@web.de>
2019-06-28 11:27:19 +0200

    Kernel and firmware related configuration changes

--- target-cfg/update_buildhost.sh
+++ target-cfg/update_buildhost.sh
@@ -2,12 +2,19 @@
 
 CFG_DIR="$(dirname $0)"
 
-TARGET_DIR="$(dirname "$CFG_DIR")"/root
+TARGET_DIR="$(dirname "$CFG_DIR")"/../root
 
 cp -av "$CFG_DIR"/base/* "$TARGET_DIR"
 cp -av "$CFG_DIR"/release/* "$TARGET_DIR"
 cp -av "$CFG_DIR"/buildhost/* "$TARGET_DIR"
 
+echo "Add packages to world"
+cat >> "$TARGET_DIR"/var/lib/portage/world <<EOL
+app-portage/cpuid2cpuflags
+app-portage/gentoolkit
+app-portage/eix
+sys-devel/distcc
+EOL
 mkdir -p "$TARGET_DIR"/usr/portage/packages
 
 echo "Configure make.conf for buildpkg and distcc"
