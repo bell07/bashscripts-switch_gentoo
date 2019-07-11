8cbf66e5ced6456a366c9d3ca00488ccfa81f40f
Alexander Weber <web.alexander@web.de>
2019-07-11 06:50:39 +0200

    adjust build_stage.sh to use app-portage/nintendo-switch-overlay and overlay profile

--- target-cfg/build_stage.sh
+++ target-cfg/build_stage.sh
@@ -7,14 +7,21 @@ TARGET_DIR="$PROJ_DIR"/out/stage3
 echo " ----- Step 1. Build base files"
 mkdir -p "$TARGET_DIR"/usr/portage
 mount -v --bind /usr/portage "$TARGET_DIR"/usr/portage
+# install overlay
 ROOT="$TARGET_DIR" PORTDIR_OVERLAY="$PROJ_DIR"/tools/switch_overlay ACCEPT_KEYWORDS='**' \
-		USE=build emerge -vq --buildpkg=n --usepkg=n sys-devel/nintendo-switch-portage-cfg
-ROOT="$TARGET_DIR" PORTAGE_CONFIGROOT="$TARGET_DIR" PORTDIR_OVERLAY="$PROJ_DIR"/tools/switch_overlay \
+		USE=build emerge -vq --buildpkg=n --usepkg=n --nodeps app-portage/nintendo-switch-overlay
+
+# set to overlay profile
+mkdir -p "$TARGET_DIR"/etc/portage/
+ln -s "$PROJ_DIR"/tools/switch_overlay/profiles/nintendo_switch/17.0/binary "$TARGET_DIR"/etc/portage/make.profile
+
+# install baselayout
+ROOT="$TARGET_DIR" PORTAGE_CONFIGROOT="$TARGET_DIR" PORTDIR_OVERLAY="$PROJ_DIR"/tools/switch_overlay FEATURES="-getbinpkg" \
 		USE=build emerge -v1q --buildpkg=n --usepkg=n baselayout
 
 echo " ----- Step 2. Install system for working portage"
 echo "... warinig about missed overlay is ok at this point"
-ROOT="$TARGET_DIR" PORTAGE_CONFIGROOT="$TARGET_DIR" PKGDIR="$PROJ_DIR"/packages \
+ROOT="$TARGET_DIR" PORTAGE_CONFIGROOT="$TARGET_DIR" PKGDIR="$PROJ_DIR"/packages FEATURES="-getbinpkg" \
 		emerge -1v --usepkgonly --with-bdeps=n --jobs=5 $(cat /usr/portage/profiles/base/packages | grep '^*' | sed 's/^*//g')
 
 echo " ----- Step 3. Re-Install system in chroot"
@@ -23,7 +30,8 @@ mkdir -p "$TARGET_DIR"/var/cache/binpkgs
 "$PROJ_DIR"/qemu-chroot.sh "$TARGET_DIR"  << EOF
 env-update
 source /etc/profile
-FEATURES="-pid-sandbox buildpkg" emerge --usepkg --with-bdeps=y -evDN --jobs=5 @system
+eselect profile set switch:nintendo_switch/17.0/binary
+FEATURES="-pid-sandbox buildpkg" emerge --usepkg --with-bdeps=y -evDN --jobs=5 @system @world
 EOF
 
 "$PROJ_DIR"/tools/system_chroot/chroot-umount.sh "$TARGET_DIR" # Be sure all is unmounted in case of errors
