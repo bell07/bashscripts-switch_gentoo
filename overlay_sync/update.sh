b90e6083304f81fddac0efb97a99e8479e1ea2fb
Alexander Weber <web.alexander@web.de>
2021-06-28 21:25:48 +0200

    overlay_sync: add menelkir overlay and sync libretro packages

--- overlay_sync/update.sh
+++ overlay_sync/update.sh
@@ -80,7 +80,7 @@ patch -p1 "$DST"/eclass/kernel-build.eclass < "$PROJ"/patches/eclass_kernel_buil
 
 # Use linux-firmware from portage
 SRC="$PORTAGE"
-do_move "sys-kernel/linux-firmware" "sys-kernel/linux-firmware"
+do_move "sys-kernel/linux-firmware"
 
 # Use onboard from wjn overlay
 SRC="$PROJ"/wjn-overlay/
@@ -90,4 +90,148 @@ rm -v "$DST"/"app-accessibility/onboard/files/"{onboard-1.2.0-remove-duplicated-
 # Use x11-misc/touchegg from waffle-builds overlay
 SRC="$PROJ"/waffle-builds/
 do_move "sys-apps/touchegg" "x11-misc/touchegg"
+
+# Libretro / Retroarch from menelkir
+SRC="$PROJ"/menelkir
+cp -v "$SRC"/eclass/libretro*  "$DST"/eclass
+
+dev-lang/rgbds
+do_move games-emulation/3dengine-libretro
+do_move games-emulation/81-libretro
+do_move games-emulation/atari800-libretro
+do_move games-emulation/bk-libretro
+do_move games-emulation/bluemsx-libretro
+do_move games-emulation/boom3-libretro
+do_move games-emulation/bsnes-libretro
+do_move games-emulation/bsnes-mercury-libretro
+do_move games-emulation/cannonball-libretro
+do_move games-emulation/cap32-libretro
+do_move games-emulation/chailove-libretro
+#do_move games-emulation/citra-libretro  - patched
+do_move games-emulation/craft-libretro
+do_move games-emulation/crocods-libretro
+do_move games-emulation/daphne-libretro 
+#do_move games-emulation/desmume-libretro - patched
+do_move games-emulation/dinothawr-libretro
+do_move games-emulation/dolphin-libretro
+do_move games-emulation/easyrpg-libretro
+do_move games-emulation/ecwolf-libretro
+do_move games-emulation/fbalpha2012-libretro
+do_move games-emulation/fbalpha-libretro
+do_move games-emulation/fbneo-libretro
+do_move games-emulation/fceumm-libretro
+do_move games-emulation/fceu-next-libretro
+do_move games-emulation/ffmpeg-libretro
+do_move games-emulation/flycast-libretro
+do_move games-emulation/fmsx-libretro
+do_move games-emulation/freechaf-libretro
+do_move games-emulation/freeintv-libretro
+do_move games-emulation/frodo-libretro
+do_move games-emulation/fuse-libretro
+do_move games-emulation/gambatte-libretro
+do_move games-emulation/gearboy-libretro
+do_move games-emulation/gearsystem-libretro
+do_move games-emulation/genesis_plus_gx-libretro
+do_move games-emulation/gme-libretro
+do_move games-emulation/gpsp-libretro
+do_move games-emulation/gw-libretro
+do_move games-emulation/handy-libretro
+do_move games-emulation/hatari-libretro
+do_move games-emulation/hbmame-libretro
+do_move games-emulation/libretro-common-overlays
+do_move games-emulation/libretro-common-shaders
+do_move games-emulation/libretro-database
+do_move games-emulation/libretro-info
+do_move games-emulation/libretro-meta
+do_move games-emulation/lutro-libretro
+do_move games-emulation/mame2000-libretro
+do_move games-emulation/mame2003-libretro
+mo_move games-emulation/mame2003_plus-libretro
+do_move games-emulation/mame2010-libretro
+do_move games-emulation/mame2015-libretro
+do_move games-emulation/mame2016-libretro
+do_move games-emulation/mame-libretro
+do_move games-emulation/mednafen-bsnes-libretro
+do_move games-emulation/mednafen-gba-libretro
+do_move games-emulation/mednafen-lynx-libretro
+do_move games-emulation/mednafen-ngp-libretro
+do_move games-emulation/mednafen-pce-fast-libretro
+do_move games-emulation/mednafen-pce-libretro
+do_move games-emulation/mednafen-pcfx-libretro
+do_move games-emulation/mednafen-psx-libretro
+do_move games-emulation/mednafen-saturn-libretro
+do_move games-emulation/mednafen-supergrafx-libretro
+do_move games-emulation/mednafen-vb-libretro
+do_move games-emulation/mednafen-wswan-libretro
+do_move games-emulation/melonds-libretro
+do_move games-emulation/meowpc98-libretro
+do_move games-emulation/mesen-libretro
+do_move games-emulation/mess2015-libretro
+do_move games-emulation/meteor-libretro
+do_move games-emulation/mgba-libretro
+do_move games-emulation/mrboom-libretro
+do_move games-emulation/mu-libretro
+do_move games-emulation/mupen64next-libretro
+do_move games-emulation/nekop2-libretro
+do_move games-emulation/neocd-libretro
+do_move games-emulation/nestopia-libretro
+do_move games-emulation/np2kai-libretro
+do_move games-emulation/nxengine-libretro
+do_move games-emulation/o2em-libretro
+do_move games-emulation/oberon-libretro
+do_move games-emulation/openlara-libretro
+do_move games-emulation/opera-libretro
+do_move games-emulation/parallel-n64-libretro
+do_move games-emulation/pcsx-rearmed-libretro
+do_move games-emulation/picodrive-libretro
+do_move games-emulation/play-libretro
+do_move games-emulation/pocketcdg-libretro
+do_move games-emulation/pokemini-libretro
+do_move games-emulation/ppsspp-libretro
+do_move games-emulation/prboom-libretro
+do_move games-emulation/prosystem-libretro
+do_move games-emulation/puae-libretro
+do_move games-emulation/px68k-libretro
+do_move games-emulation/quasi88-libretro
+do_move games-emulation/quicknes-libretro
+do_move games-emulation/race-libretro
+do_move games-emulation/redbook-libretro
+do_move games-emulation/reminiscence-libretro
+do_move games-emulation/retroarch
+do_move games-emulation/retroarch-assets
+do_move games-emulation/retroarch-joypad-autoconfig
+do_move games-emulation/retrodream-libretro
+do_move games-emulation/sameboy-libretro
+do_move games-emulation/scummvm-libretro
+do_move games-emulation/snes9x2002-libretro
+do_move games-emulation/snes9x2005-libretro
+do_move games-emulation/snes9x2010-libretro
+do_move games-emulation/snes9x-libretro
+do_move games-emulation/squirreljme-libretro
+do_move games-emulation/stella2014-libretro
+do_move games-emulation/stella-libretro
+do_move games-emulation/tgbdual-libretro
+do_move games-emulation/theodore-libretro
+do_move games-emulation/tyrquake-libretro
+do_move games-emulation/uzem-libretro
+do_move games-emulation/vbam-libretro
+do_move games-emulation/vba-next-libretro
+do_move games-emulation/vecx-libretro
+do_move games-emulation/vemulator-libretro
+do_move games-emulation/vice-x128-libretro
+do_move games-emulation/vice-x64-libretro
+do_move games-emulation/vice-x64sc-libretro
+do_move games-emulation/vice-xcbm2-libretro
+do_move games-emulation/vice-xcbm5x0-libretro
+do_move games-emulation/vice-xpet-libretro
+do_move games-emulation/vice-xplus4-libretro
+do_move games-emulation/vice-xscpu64-libretro
+do_move games-emulation/vice-xvic-libretro
+do_move games-emulation/virtualjaguar-libretro
+do_move games-emulation/vitaquake2-libretro
+do_move games-emulation/vitaquake3-libretro
+do_move games-emulation/vitavoyager-libretro
+do_move games-emulation/x1-libretro
+do_move games-emulation/xrick-libretro
+do_move games-emulation/yabause-libretro
 
