cacbd837b9f8cad1afeacf09ed5e2179ed4e45f5
Alexander Weber <web.alexander@web.de>
2021-07-07 07:57:28 +0200

    Add docker pull to buildscripts/build_bootstack_docker.sh

--- buildscripts/build_bootstack_docker.sh
+++ buildscripts/build_bootstack_docker.sh
@@ -2,11 +2,13 @@
 CFG_DIR="$(realpath "$(dirname $0)")"
 PROJ_DIR="$(dirname "$CFG_DIR")"
 BUILD_DIR="$PROJ_DIR"/bootstack-build
+DOCKER_IMG=registry.gitlab.com/switchroot/bootstack/bootstack-build-scripts
 
 /etc/init.d/docker start
 
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 
-docker run -it --rm -e CPUS=$(nproc) -e DISTRO=gentoo -v "${PWD}"/out:/out registry.gitlab.com/switchroot/bootstack/bootstack-build-scripts
+docker pull "$DOCKER_IMG"
+docker run -it --rm -e CPUS=$(nproc) -e DISTRO=gentoo -v "${PWD}"/out:/out "$DOCKER_IMG"
 mv -v "$BUILD_DIR"/out/switchroot-gentoo-boot* "$PROJ_DIR"/distfiles
